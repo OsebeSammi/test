4c50b92896fe1d5fd309cfa38b53c4c418683520
FELIX <felixkimaru@gmail.com>
2013-12-22 01:41:03 +0300

    weeks view finalized

--- views/weeks.php
+++ views/weeks.php
@@ -3,92 +3,104 @@
 	    <div class="container">
 			<div class="row">
 				<div class="col-xs-12 col-sm-12">
+			    	<ol class="breadcrumb">
+					  <li><a href="#">Home</a></li>
+					  <li><a href="#">Library</a></li>
+					  <li class="active">Data</li>
+					</ol>
 			    	<h1>Browse Courses by Week</h1>
+			    	
 				</div>
 			</div>
 	    </div> <!-- /container -->
+	     <div class="container">
+	     	<div class="row">
+	     		<div class="col-xs-12 col-sm-12">
+	     			Sort by <a href="">Weeks </a>| <a href="">Topics </a>
+	     		</div>
+	     		
+	     	</div>
+	     </div>
+	     
 	    <div class="container">
 	    	<div class="lessons-wrap">
 	    		<div class="class-thumbs-wrap">
 			    	<div class="row">
-			    		<div class="col-xs-12 col-sm-12">
-			    			<div class="coursthumbs">
-			    				<ul class="nopadding nomargin listnone listing-view">
-			    					<li class="less-listing course-thumbs">
-			    						<div class="bg-arrow">
-			    							<a href="/content.php">
-					    						<div class="less-title">
-					    							Week [ ]
-					    						</div>
-					    						<p>Nulla vitae elit libero, a pharetra augue. Nulla vitae elit libero, a pharetra augue. </p>
-				    						 </a>
-			    						</div>
-			    					</li>
-			    					<li class="less-listing course-thumbs">
-			    						<div class="bg-arrow">
-			    							<a href="/content.php">
-					    						<div class="less-title">
-					    							Week [ ]
-					    						</div>
-					    						<p>Nulla vitae elit libero, a pharetra augue. Nulla vitae elit libero, a pharetra augue. </p>
-				    						 </a>
-			    						</div>
-			    					</li>
-			    					<li class="less-listing course-thumbs">
-			    						<div class="bg-arrow">
-			    							<a href="/content.php">
-					    						<div class="less-title">
-					    							Week [ ]
-					    						</div>
-					    						<p>Nulla vitae elit libero, a pharetra augue. Nulla vitae elit libero, a pharetra augue. </p>
-				    						 </a>
-			    						</div>
-			    					</li>
-			    					<li class="less-listing course-thumbs">
-			    						<div class="bg-arrow">
-			    							<a href="/content.php">
-					    						<div class="less-title">
-					    							Week [ ]
-					    						</div>
-					    						<p>Nulla vitae elit libero, a pharetra augue. Nulla vitae elit libero, a pharetra augue. </p>
-				    						 </a>
-			    						</div>
-			    					</li>
-			    					<li class="less-listing course-thumbs">
-			    						<div class="bg-arrow">
-			    							<a href="/content.php">
-					    						<div class="less-title">
-					    							Week [ ]
-					    						</div>
-					    						<p>Nulla vitae elit libero, a pharetra augue. Nulla vitae elit libero, a pharetra augue. </p>
-				    						 </a>
-			    						</div>
-			    					</li>
-			    					<li class="less-listing course-thumbs">
-			    						<div class="bg-arrow">
-			    							<a href="/content.php">
-					    						<div class="less-title">
-					    							Week [ ]
-					    						</div>
-					    						<p>Nulla vitae elit libero, a pharetra augue. Nulla vitae elit libero, a pharetra augue. </p>
-				    						 </a>
-			    						</div>
-			    					</li>
-			    					<li class="less-listing course-thumbs">
-			    						<div class="bg-arrow">
-			    							<a href="/content.php">
-					    						<div class="less-title">
-					    							Week [ ]
-					    						</div>
-					    						<p>Nulla vitae elit libero, a pharetra augue. Nulla vitae elit libero, a pharetra augue. </p>
-				    						 </a>
-			    						</div>
-			    					</li>
+			    		<div class="col-xs-4 col-sm-4">
+			    			<div class="coursebyweek">
+			    				<ul class="nopadding nomargin listnone">
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
 			    				</ul>
 			    			</div>
 			    		</div>
-			    		<div class="col-xs-12 col-sm-12">
-			    			<p class="center nomargin"> <button type="submit" class="btn btn-default long-btn"><span class="glyphicon glyphicon-search"></span> Show more</button></p>
+			    		<div class="col-xs-4 col-sm-4">
+			    			<div class="coursthumbs">
+			    				<ul class="nopadding nomargin listnone">
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    				</ul>
+			    			</div>
+			    		</div>
+			    		<div class="col-xs-4 col-sm-4">
+			    			<div class="coursthumbs">
+			    				<ul class="nopadding nomargin listnone">
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					<li><a href="/weekcontents.php">Week 1</a></li>
+			    					
+			    				</ul>
+			    			</div>
 			    		</div>
 			    		
 			    		
